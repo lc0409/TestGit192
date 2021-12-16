ec039073fea95c37f5af8e8da4059028ff36e002
lc0409 <1091766795@qq.com>
2021-12-16 21:02:59 +0800

    Initial commit

---
+++ src/TestGit192.java
@@ -0,0 +1,8 @@
+public class TestGit192 {
+    public static void main(String[] args){
+    }
+
+    public static void stuInfo(String sid, String sname){
+        System.out.println("学号：" + sid + "姓名：" + sname);
+    }
+}
